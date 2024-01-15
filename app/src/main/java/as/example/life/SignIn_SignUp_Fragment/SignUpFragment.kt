f0bb78baf27fe2990612967b5f0366df7c22122c
rahulverma41328 <rahulverma41328@gmail.com>
2024-01-15 21:07:03 +0530

    code of signup

--- app/src/main/java/as/example/life/SignIn_SignUp_Fragment/SignUpFragment.kt
+++ app/src/main/java/as/example/life/SignIn_SignUp_Fragment/SignUpFragment.kt
@@ -5,28 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.widget.AppCompatEditText
 import `as` .example.life.R
+import `as`.example.life.databinding.FragmentEventBinding
+import `as`.example.life.databinding.FragmentSignUpBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SignUpFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SignUpFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var binding:FragmentSignUpBinding
+    private lateinit var userName:AppCompatEditText
+    private lateinit var userEmail:AppCompatEditText
+    private lateinit var userPassword:AppCompatEditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
         }
     }
 
@@ -34,26 +29,23 @@ class SignUpFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+        binding = FragmentSignUpBinding.inflate(inflater, container, false)
+        userName = binding.userUsername
+        userEmail = binding.userEmail
+        userPassword = binding.userPassword
+        var userNameSt:String = userName.text.toString()
+        var userEmailSt:String = userEmail.text.toString()
+        var userPasswordSt:String = userPassword.text.toString()
+        
+        return binding.root
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SignUpFragment.
-         */
-        // TODO: Rename and change types and number of parameters
+
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             SignUpFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
                 }
             }
     }
